f695fbdb0d41d1231e602fdba4eca5f416c0b2aa
Alecx Moritz <hanelore@live.com>
2019-01-25 20:10:59 -0500

    3.4 - added drawer and app bar - reformated home

--- src/components/Main/Home.js
+++ src/components/Main/Home.js
@@ -104,83 +104,80 @@ class Home extends React.Component {
         const { open } = this.state;
         return (
             <BrowserRouter>
-                <div>
-                    <h2>Home</h2>
-                    <button onClick={this.props.logout}>Log Out</button>
-                    <hr />
-                    <Link to="/profile"><button>Profile</button></Link>
-                    <Link to="/"><button>Feed</button></Link>
-                    <Switch>
-                        <Route path="/profile"><Images /></Route>
-                        <Route path="/"><Feed /></Route>
-                    </Switch>
-
-                    <div className={classes.root}>
-                        <CssBaseline />
-                        <AppBar
-                            position="fixed"
-                            className={classNames(classes.appBar, {
-                                [classes.appBarShift]: open,
-                            })}
-                        >
-                            <Toolbar disableGutters={!open}>
-                                <IconButton
-                                    color="inherit"
-                                    aria-label="Open drawer"
-                                    onClick={this.handleDrawerOpen}
-                                    className={classNames(classes.menuButton, open && classes.hide)}
-                                >
-                                    <MenuIcon />
-                                </IconButton>
-                                <Typography variant="h6" color="inherit" noWrap>
-                                    Persistent drawer
+                <div className={classes.root}>
+                    <CssBaseline />
+                    <AppBar
+                        position="fixed"
+                        className={classNames(classes.appBar, {
+                            [classes.appBarShift]: open,
+                        })}
+                    >
+                        <Toolbar disableGutters={!open}>
+                            <IconButton
+                                color="inherit"
+                                aria-label="Open drawer"
+                                onClick={this.handleDrawerOpen}
+                                className={classNames(classes.menuButton, open && classes.hide)}
+                            >
+                                <MenuIcon />
+                            </IconButton>
+                            <Typography variant="h6" color="inherit" noWrap>
+                                IMAGES
             </Typography>
-                            </Toolbar>
-                        </AppBar>
-                        <Drawer
-                            className={classes.drawer}
-                            variant="persistent"
-                            anchor="left"
-                            open={open}
-                            classes={{
-                                paper: classes.drawerPaper,
-                            }}
-                        >
-                            <div className={classes.drawerHeader}>
-                                <IconButton onClick={this.handleDrawerClose}>
-                                    {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
-                                </IconButton>
-                            </div>
-                            <Divider />
-                            <List>
-                                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-                                    <ListItem button key={text}>
-                                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                                        <ListItemText primary={text} />
-                                    </ListItem>
-                                ))}
-                            </List>
-                            <Divider />
-                            <List>
-                                {['All mail', 'Trash', 'Spam'].map((text, index) => (
-                                    <ListItem button key={text}>
-                                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                                        <ListItemText primary={text} />
-                                    </ListItem>
-                                ))}
-                            </List>
-                        </Drawer>
-                        <main
-                            className={classNames(classes.content, {
-                                [classes.contentShift]: open,
-                            })}
-                        >
-                            <div className={classes.drawerHeader} />
-                            <h1>CONTENT</h1>
-                        </main>
-                    </div>
-
-
+                        </Toolbar>
+                    </AppBar>
+                    <Drawer
+                        className={classes.drawer}
+                        variant="persistent"
+                        anchor="left"
+                        open={open}
+                        classes={{
+                            paper: classes.drawerPaper,
+                        }}
+                    >
+                        <div className={classes.drawerHeader}>
+                            <IconButton onClick={this.handleDrawerClose}>
+                                {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+                            </IconButton>
+                        </div>
+                        <Divider />
+                        <List>
+                            <ListItem button onClick={this.props.logout}>
+                                <ListItemIcon><InboxIcon /></ListItemIcon>
+                                <ListItemText primary={"Logout"} />
+                            </ListItem>
+                            <Link to="/profile">
+                                <ListItem button>
+                                    <ListItemIcon><InboxIcon /></ListItemIcon>
+                                    <ListItemText primary={"Profile"} />
+                                </ListItem>
+                            </Link>
+                            <Link to="/">
+                                <ListItem button>
+                                    <ListItemIcon><InboxIcon /></ListItemIcon>
+                                    <ListItemText primary={"Feed"} />
+                                </ListItem>
+                            </Link>
+                        </List>
+                        <Divider />
+                        <List>
+                            <ListItem button>
+                                <ListItemIcon><InboxIcon /></ListItemIcon>
+                                <ListItemText primary={"Post New"} />
+                            </ListItem>
+                        </List>
+                    </Drawer>
+                    <main
+                        className={classNames(classes.content, {
+                            [classes.contentShift]: open,
+                        })}
+                    >
+                        <div className={classes.drawerHeader} />
+                        <Switch>
+                            <Route path="/profile"><Images /></Route>
+                            <Route path="/"><Feed /></Route>
+                        </Switch>
+                    </main>
                 </div>
             </BrowserRouter>
         )
